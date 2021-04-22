e1a48e78a6df98c792db65d5975ff74725799439
jypark <jypark@CKLSELUSR865.CKLINE.CO.KR>
2021-04-22 16:08:26 +0900

    Inital Project

---
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,30 @@
+package com.jojoldu.book.springboot.service.posts;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+	private final PostsRepository postsRepository;
+	
+	@Transactional
+	public Long save(PostsSaveRequestDto requestDto) {
+		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+	
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		
+		
+		return id;
+	}
+}
---
+++ freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+	private final PostsService postsService;
+	
+	@PostMapping("/api/v1/posts")
+	public Long Save(@RequestBody PostsSaveRequestDto requestDto) {
+		return postsService.save(requestDto);
+	}
+	
+	//@PutMapping("/api/v1/posts/{id}")
+	//public Long update(@PathVariable Long id, @RequestBody PostsUpdat)
+	
+	
+	//@GetMapping("/api/v1/posts/{id}")
+	
+}
